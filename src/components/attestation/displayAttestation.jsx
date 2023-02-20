a146c606a3834e402a6094c63b9a0229c88b7c73
Houssam Mrabet <dodslim100@gmail.com>
2023-02-20 06:19:43 +0100

    edit displayAtt component style, set the print functionality

--- src/components/attestation/displayAttestation.jsx
+++ src/components/attestation/displayAttestation.jsx
@@ -5,8 +5,119 @@ import "./attestation.scss"
 
 const DisplayAtt = () => {
   return (
-    <div className='component col'>
-      <img src={img} alt="" />
+    <div className='printer'>
+      <div className='component col att'>
+        <img src={img} alt="" />
+        <h5 className='my-1' style={{fontSize: "18px"}}>الاكاديمية الجهوية للتربية و التكوين لجهة الدار البيضاء - سطات</h5>
+        <h5 className='my-1' style={{fontSize: "18px"}}>المديرية الاقليمية لبنسليمان</h5>
+        <h5 className='my-1' style={{fontSize: "18px"}}>الثانوية التأهيلية الشريف الإدريسي</h5>
+        <h6 className='my-1'>بنسليمان 023291593</h6>
+        <h6 className='bordred-title my-1'><span className='attNumTitle'>شهادة مدرسية رقم: </span><span className='attNum mx-4'>22/23/446</span></h6>
+        <h6 className='my-3'>يشهد : مدير الثانوية التأهيلية الشريف الإدريسي ببنسليمان</h6>
+        <div className='attRow my-2'>
+          <div className=''>
+            <span>بأن التلميذ(ة): </span><span className='value'>حسام امرابط</span>
+          </div>
+          <div className=''>
+            <span>رقم التسجيل: </span><span className='value'>حسام امرابط</span>
+          </div>
+        </div>
+        <div className='attRow my-2'>
+          <div>
+            <span>المزداد(ة) في: </span><span className='value'>حسام امرابط</span>
+          </div>
+          <div className='secondEl'>
+            <span>بتاريخ: </span><span className='value'>حسام امرابط</span>
+          </div>
+        </div>
+        <div className='singleRow my-2'>
+            <span>كان(ت) يتابع دراسته(ها) بمستوى: </span><span className='value'>حسام امرابط</span>
+        </div>
+        <div className='singleRow my-2'>
+            <span>و قد انقطع عن الدراسة بتاريخ: </span><span className='value'>حسام امرابط</span>
+        </div>
+        <div className='attRow my-2'>
+          <div>
+            <span>الرقم الوطني: </span><span className='value'>حسام امرابط</span>
+          </div>
+          <div className='secondEl'>
+            <span>الموسم الدراسي: </span><span className='value'>حسام امرابط</span>
+          </div>
+        </div>
+        <div className='singleRow my-2'>
+            <span>ملاحظات: </span><span className='value'>حسام امرابط</span>
+        </div>
+        <div className='singleRow my-2'>
+            <span style={{fontWeight: "bolder", fontSize: "14px"}}>ملحوظة : سلمت هذه الشهادة لغرض اداري ولا تخول التسجيل في مؤسسة أخرى</span>
+        </div>
+        <div className='singleRow-R my-2'>
+          <span>بنسليمان في: </span><span className='value'>حسام امرابط</span>
+        </div>
+        <div className='attRow-E my-2 mb-4'>
+          <div>
+            <span>المستندات</span>
+          </div>
+          <div className='secondEl'>
+            <span>رئيس المؤسسة</span>
+          </div>
+        </div>
+      </div>
+      <div className='component col att second'>
+        <img src={img} alt="" />
+        <h5 className='my-1' style={{fontSize: "18px"}}>الاكاديمية الجهوية للتربية و التكوين لجهة الدار البيضاء - سطات</h5>
+        <h5 className='my-1' style={{fontSize: "18px"}}>المديرية الاقليمية لبنسليمان</h5>
+        <h5 className='my-1' style={{fontSize: "18px"}}>الثانوية التأهيلية الشريف الإدريسي</h5>
+        <h6 className='my-1'>بنسليمان 023291593</h6>
+        <h6 className='bordred-title my-1'><span className='attNumTitle'>شهادة مدرسية رقم: </span><span className='attNum mx-4'>22/23/446</span></h6>
+        <h6 className='my-3'>يشهد : مدير الثانوية التأهيلية الشريف الإدريسي ببنسليمان</h6>
+        <div className='attRow my-2'>
+          <div className=''>
+            <span>بأن التلميذ(ة): </span><span className='value'>حسام امرابط</span>
+          </div>
+          <div className=''>
+            <span>رقم التسجيل: </span><span className='value'>حسام امرابط</span>
+          </div>
+        </div>
+        <div className='attRow my-2'>
+          <div>
+            <span>المزداد(ة) في: </span><span className='value'>حسام امرابط</span>
+          </div>
+          <div className='secondEl'>
+            <span>بتاريخ: </span><span className='value'>حسام امرابط</span>
+          </div>
+        </div>
+        <div className='singleRow my-2'>
+            <span>كان(ت) يتابع دراسته(ها) بمستوى: </span><span className='value'>حسام امرابط</span>
+        </div>
+        <div className='singleRow my-2'>
+            <span>و قد انقطع عن الدراسة بتاريخ: </span><span className='value'>حسام امرابط</span>
+        </div>
+        <div className='attRow my-2'>
+          <div>
+            <span>الرقم الوطني: </span><span className='value'>حسام امرابط</span>
+          </div>
+          <div className='secondEl'>
+            <span>الموسم الدراسي: </span><span className='value'>حسام امرابط</span>
+          </div>
+        </div>
+        <div className='singleRow my-2'>
+            <span>ملاحظات: </span><span className='value'>حسام امرابط</span>
+        </div>
+        <div className='singleRow my-2'>
+            <span style={{fontWeight: "bolder", fontSize: "14px"}}>ملحوظة : سلمت هذه الشهادة لغرض اداري ولا تخول التسجيل في مؤسسة أخرى</span>
+        </div>
+        <div className='singleRow-R my-2'>
+          <span>بنسليمان في: </span><span className='value'>حسام امرابط</span>
+        </div>
+        <div className='attRow-E my-2 mb-4'>
+          <div>
+            <span>المستندات</span>
+          </div>
+          <div className='secondEl'>
+            <span>رئيس المؤسسة</span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
