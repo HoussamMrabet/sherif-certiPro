3d3b6ee92a8f804871e6c4b77e42fb227f6993da
Houssam Mrabet <52783782+HoussamMrabet@users.noreply.github.com>
2023-04-24 12:21:12 +0000

    Update firebase-config.js

--- src/data/firebase-config.js
+++ src/data/firebase-config.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDbMmSh-EVI7dkhQ89P1EqhFQcznexBhsU",
-  authDomain: "crud-8d516.firebaseapp.com",
-  projectId: "crud-8d516",
-  storageBucket: "crud-8d516.appspot.com",
-  messagingSenderId: "811238590978",
-  appId: "1:811238590978:web:ebdae52a3b820062c3351b",
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
